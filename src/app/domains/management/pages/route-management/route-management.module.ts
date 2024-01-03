dd923fc5c72d8e22611d0805c52a4551cf0d3017
LeeH109 <hanhnguyen.10092002@gmail.com>
2024-01-04 00:37:01 +0700

    filter route

--- src/app/domains/management/pages/route-management/route-management.module.ts
+++ src/app/domains/management/pages/route-management/route-management.module.ts
@@ -7,17 +7,23 @@ import { NgxRerenderModule } from "ngx-rerender";
 import { PendingRouteModule } from "../../components/route/pending-route/pending-route.module";
 import { DialogService, DynamicDialogModule } from "primeng/dynamicdialog";
 import { PendingRouteDetailModule } from "../../components/route/pending-route-detail/pending-route-detail.module";
-
+import { CalendarModule } from "primeng/calendar";
+import { DropdownModule } from "primeng/dropdown";
+import { FormsModule } from "@angular/forms";
 @NgModule({
   declarations: [RouteManagementComponent],
   imports: [
     CommonModule,
+    CalendarModule,
+    FormsModule,
+    DropdownModule,
     RouteFormModule,
     RouteListboxModule,
     NgxRerenderModule,
     PendingRouteModule,
     DynamicDialogModule,
     PendingRouteDetailModule,
+  
   ],
   exports: [RouteManagementComponent],
   providers: [DialogService],
